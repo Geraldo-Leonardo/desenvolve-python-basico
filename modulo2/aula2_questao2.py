f23a98417f28e1378fa0bf3e94450b5b4c9a856d
Geraldo Leonardo Ribeiro <geraldo_iba@yahoo.com.br>
2025-10-08 21:13:07 -0300

    Respostas exercícios modulo2

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,19 @@
+"""""
+Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra 
+variável, e o valor 3.5 numa terceira variável.
+Some os valores da segunda e terceira variável e armazene em outra variável.
+Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+"""
+
+texto = "O resultado é :"
+num1 = 10
+num2 = 3.5
+soma = num1 + num2
+print(texto)
+print(num1)
+print(num2)
+print(soma)
+print(type(texto))
+print(type(num1))
+print(type(num2))
+print(type(soma))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,15 @@
+"""
+juros = 1.01
+saldo = 500.0
+rendimento1 = saldo * juros
+rendimento2 = rendimento1 * juros
+rendimento3 = rendimento2 * juros
+
+print("Após 3 meses meu novo saldo é")
+print(rendimento3)
+"""
+
+juros = 1.01
+saldo_rendimento3 = 500.0 *1.01 * 1.01 * 1.01
+print("Após 3 meses meu novo saldo é :")
+print(saldo_rendimento3)
